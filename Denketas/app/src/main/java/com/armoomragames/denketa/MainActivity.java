7eaf1b560742914968aff91f2d95fdd5a7ba8b5c
Usama <usamak.mirza@gmail.com>
2021-08-21 16:42:25 +0500

    Added all nessasary files

--- Denketas/app/src/main/java/com/armoomragames/denketa/MainActivity.java
+++ Denketas/app/src/main/java/com/armoomragames/denketa/MainActivity.java
@@ -1,14 +1,42 @@
 package com.armoomragames.denketa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+import com.armoomragames.denketa.Utils.AppConstt;
+import com.armoomragames.denketa.Utils.IBadgeUpdateListener;
+
+public class MainActivity extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        AppConfig.getInstance().performLangCheck(getWindow());
+        if (savedInstanceState != null) {
+            return;
+        }
+
+
     }
+
+
+
 }
