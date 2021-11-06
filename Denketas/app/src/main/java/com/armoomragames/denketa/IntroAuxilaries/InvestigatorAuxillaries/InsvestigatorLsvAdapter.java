8bd4294fcec73fa909a34c00fb116c324b17f939
Usama <usamak.mirza@gmail.com>
2021-11-06 19:50:35 +0500

    Make Danetka added

---
+++ Denketas/app/src/main/java/com/armoomragames/denketa/IntroAuxilaries/InvestigatorAuxillaries/InsvestigatorLsvAdapter.java
@@ -0,0 +1,95 @@
+package com.armoomragames.denketa.IntroAuxilaries.InvestigatorAuxillaries;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.armoomragames.denketa.IntroAuxilaries.PlayAuxillairies.DModel_MyDenketa;
+import com.armoomragames.denketa.R;
+import com.armoomragames.denketa.Utils.IAdapterCallback;
+
+import java.util.ArrayList;
+
+public class InsvestigatorLsvAdapter extends BaseAdapter {
+
+    private IAdapterCallback iAdapterCallback;
+    private LayoutInflater inflater;
+    private ArrayList<DModel_MyDenketa> mData;
+    private float cornerRadius;
+    private Context context;
+
+    public InsvestigatorLsvAdapter(IAdapterCallback iAdapterCallback, Context context, ArrayList<DModel_MyDenketa> mData) {
+        this.iAdapterCallback = iAdapterCallback;
+        inflater = LayoutInflater.from(context);
+        this.mData = mData;
+        this.cornerRadius = dpToPx(context, 15);
+        this.context = context;
+    }
+    public int dpToPx(Context context, int dp) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        context = parent.getContext();
+
+        final ViewHolder viewHolder;
+
+        if (convertView == null) {
+
+            convertView = inflater.inflate(R.layout.lay_item_investigator, null);
+
+            viewHolder = new ViewHolder();
+
+            viewHolder.txvName  = convertView.findViewById(R.id.lay_item_my_denekta_txvName);
+            viewHolder.imvResults = convertView.findViewById(R.id.lay_item_my_denekta_imvResults);
+
+
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.txvName.setText(mData.get(position).getStrName());
+        convertView.setOnClickListener(v -> iAdapterCallback.onAdapterEventFired(IAdapterCallback.EVENT_A,position ));
+        viewHolder.imvResults.setOnClickListener(v -> iAdapterCallback.onAdapterEventFired(IAdapterCallback.EVENT_B,position ));
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView txvName;
+        ImageView imvResults;
+    }
+
+
+    public void filterList(ArrayList<DModel_MyDenketa> filterllist) {
+        // below line is to add our filtered
+        // list in our course array list.
+        mData = filterllist;
+        // below line is to notify our adapter
+        // as change in recycler view data.
+        notifyDataSetChanged();
+    }
+}
--- Denketas/app/src/main/java/com/armoomragames/denketa/IntroAuxilaries/PlayAuxillairies/MakeDenketaFragment.java
+++ Denketas/app/src/main/java/com/armoomragames/denketa/IntroAuxilaries/PlayAuxillairies/MakeDenketaFragment.java
@@ -2,29 +2,43 @@ package com.armoomragames.denketa.IntroAuxilaries.PlayAuxillairies;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.armoomragames.denketa.AppConfig;
 import com.armoomragames.denketa.R;
 import com.armoomragames.denketa.Utils.CustomToast;
+import com.bumptech.glide.Glide;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import static com.armoomragames.denketa.Utils.IAdapterCallback.EVENT_A;
 
 public class MakeDenketaFragment extends Fragment implements View.OnClickListener {
     private static final int CAMERA_REQUEST = 1888;
@@ -34,9 +48,19 @@ public class MakeDenketaFragment extends Fragment implements View.OnClickListene
     String encodedImage;
     Bitmap selectedImageBitmap = null;
     LinearLayout insert1, insert2;
-    ImageView imvInsert1,imvInsert2;
-    boolean isFirst=false;
-    boolean isSecond=false;
+    ImageView imvInsert1, imvInsert2;
+    ImageView imvAddMainRegitlto, imvAddSingle;
+    RecyclerView rcvRegilto;
+    boolean isFirst = false;
+    boolean isSecond = false;
+    EditText edtRegilto;
+    RelativeLayout rlRegiltoitem;
+    LinearLayout llSubmit;
+    TextView txvSubmit;
+    ArrayList<String> lstRegilto;
+    RegiltoRCVAdapter regiltoRCVAdapter;
+    EditText edtTitle, edtRidle, edtAns;
+    private Dialog progressDialog;
 
     public static Fragment newInstance(int position) {
         Fragment frag = new MakeDenketaFragment();
@@ -49,12 +73,37 @@ public class MakeDenketaFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View frg = inflater.inflate(R.layout.fragment_make_denketa, container, false);
+
+        init();
         bindviews(frg);
+
+        lstRegilto.add("Press Add.. Example");
+        populatePopulationList();
         return frg;
     }
 
-    private void bindviews(View frg)
-    {
+    private void init() {
+        lstRegilto = new ArrayList<>();
+    }
+
+    private void bindviews(View frg) {
+        rcvRegilto = frg.findViewById(R.id.frg_make_rcv_regilto);
+        imvAddMainRegitlto = frg.findViewById(R.id.frg_make_imv_addRegiltoMain);
+        edtRegilto = frg.findViewById(R.id.frg_make_edtRegilto);
+
+
+        edtTitle = frg.findViewById(R.id.frg_make_edtTitle);
+        edtRidle = frg.findViewById(R.id.frg_make_edtRidle);
+        edtAns = frg.findViewById(R.id.frg_make_edtAnswer);
+
+
+        rlRegiltoitem = frg.findViewById(R.id.frg_make_rlRegiltoItem);
+        imvAddSingle = frg.findViewById(R.id.frg_make_imvRegilto);
+
+        llSubmit = frg.findViewById(R.id.frg_make_llSubmit);
+        txvSubmit = frg.findViewById(R.id.frg_make_txvSubmit);
+
+
         insert1 = frg.findViewById(R.id.frg_make_llInsert1);
         insert2 = frg.findViewById(R.id.frg_make_llInsert2);
         imvInsert1 = frg.findViewById(R.id.frg_make_inset1Imv);
@@ -63,6 +112,9 @@ public class MakeDenketaFragment extends Fragment implements View.OnClickListene
 
         insert1.setOnClickListener(this);
         insert2.setOnClickListener(this);
+        imvAddSingle.setOnClickListener(this);
+        imvAddMainRegitlto.setOnClickListener(this);
+        llSubmit.setOnClickListener(this);
     }
 
     @Override
@@ -70,16 +122,114 @@ public class MakeDenketaFragment extends Fragment implements View.OnClickListene
         switch (v.getId()) {
 
             case R.id.frg_make_llInsert1:
-                isFirst=true; isSecond=false;
+                isFirst = true;
+                isSecond = false;
                 takePhoto();
                 break;
             case R.id.frg_make_llInsert2:
-                isFirst=false; isSecond=true;
+                isFirst = false;
+                isSecond = true;
                 takePhoto();
+                break;
+            case R.id.frg_make_llSubmit:
+
+
+                if (!edtTitle.getText().toString().equals("") && !edtRidle.getText().toString().equals("") && !edtAns.getText().toString().equals("")) {
+                    showProgDialog();
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //do something
+                            dismissProgDialog();
+                            llSubmit.setBackground(getActivity().getResources().getDrawable(R.drawable.shp_rect_rounded_app_green));
+                            txvSubmit.setText("Submitted");
+                        }
+                    }, 5000);//time in milisecond
+                } else
+                    CustomToast.showToastMessage(getActivity(), "Please fill all fields", Toast.LENGTH_LONG);
+
+                break;
+
+            case R.id.frg_make_imv_addRegiltoMain:
+
+                if (lstRegilto.size() <= 4) {
+                    rlRegiltoitem.setVisibility(View.VISIBLE);
+                    imvAddMainRegitlto.setVisibility(View.GONE);
+                } else
+                    CustomToast.showToastMessage(getActivity(), "Maximum Added", Toast.LENGTH_LONG);
+                break;
+            case R.id.frg_make_imvRegilto:
+                if (lstRegilto.size() <= 4) {
+                    if (!edtRegilto.getText().toString().equals("")) {
+                        lstRegilto.add(edtRegilto.getText().toString());
+                        populatePopulationList();
+                        rlRegiltoitem.setVisibility(View.GONE);
+                        edtRegilto.setText("");
+                        imvAddMainRegitlto.setVisibility(View.VISIBLE);
+                        AppConfig.getInstance().closeKeyboard(getActivity());
+                    } else
+                        CustomToast.showToastMessage(getActivity(), "Enter some text", Toast.LENGTH_LONG);
+
+                } else
+                    CustomToast.showToastMessage(getActivity(), "Maximum Added", Toast.LENGTH_LONG);
                 break;
         }
     }
 
+
+    private void populatePopulationList() {
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        if (regiltoRCVAdapter == null) {
+
+            regiltoRCVAdapter = new RegiltoRCVAdapter(getActivity(), lstRegilto, (eventId, position) -> {
+
+                switch (eventId) {
+                    case EVENT_A:
+
+
+                        break;
+                }
+
+            });
+
+
+            rcvRegilto.setLayoutManager(linearLayoutManager);
+            rcvRegilto.setAdapter(regiltoRCVAdapter);
+
+        } else {
+            regiltoRCVAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void dismissProgDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
+    }
+
+    private void showProgDialog() {
+
+        progressDialog = new Dialog(getActivity(), R.style.AppTheme);
+//        progressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        progressDialog.setContentView(R.layout.dialog_progress_loading);
+        WindowManager.LayoutParams wmlp = progressDialog.getWindow().getAttributes();
+        wmlp.gravity = Gravity.CENTER | Gravity.CENTER;
+        wmlp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        wmlp.height = ViewGroup.LayoutParams.MATCH_PARENT;
+
+        ImageView imageView = progressDialog.findViewById(R.id.img_anim);
+        Glide.with(getContext()).asGif().load(R.raw.loading).into(imageView);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+
+    }
 
     //region PermissionsAccess
     //region Permissions
@@ -95,13 +245,13 @@ public class MakeDenketaFragment extends Fragment implements View.OnClickListene
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            if (isFirst&& !isSecond){
+            if (isFirst && !isSecond) {
                 imvInsert1.setImageBitmap(selectedImageBitmap);
                 imvInsert1.setVisibility(View.VISIBLE);
 
             }
 
-            if (!isFirst&& isSecond){
+            if (!isFirst && isSecond) {
                 imvInsert2.setImageBitmap(selectedImageBitmap);
                 imvInsert2.setVisibility(View.VISIBLE);
 
