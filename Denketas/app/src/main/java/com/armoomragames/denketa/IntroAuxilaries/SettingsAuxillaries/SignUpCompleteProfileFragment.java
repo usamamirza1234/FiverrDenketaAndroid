61b924e608f27bcdc092eb7c7ebe6c101709127d
Usama <usamak.mirza@gmail.com>
2022-01-09 14:16:41 +0500

    base url changed and country is added

--- Denketas/app/src/main/java/com/armoomragames/denketa/IntroAuxilaries/SettingsAuxillaries/SignUpCompleteProfileFragment.java
+++ Denketas/app/src/main/java/com/armoomragames/denketa/IntroAuxilaries/SettingsAuxillaries/SignUpCompleteProfileFragment.java
@@ -38,6 +38,7 @@ import com.google.gson.JsonObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -59,7 +60,7 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
     IBadgeUpdateListener mBadgeUpdateListener;
     Calendar todayCalender;
 
-    List<String> lstNationalities;
+//    List<String> lstNationalities;
     List<String> lstGender;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -105,11 +106,13 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
 
         setToolbar();
 
-        lstNationalities = new ArrayList<>();
 
-        lstNationalities.add("German");
-        lstNationalities.add("US");
-        lstNationalities.add("Select Nationality");
+
+//        lstNationalities = new ArrayList<>();
+//
+//        lstNationalities.add("German");
+//        lstNationalities.add("US");
+//        lstNationalities.add("Select Nationality");
 
         lstGender = new ArrayList<>();
 
@@ -155,12 +158,12 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
         txv_dob.setOnClickListener(this);
 
 
-        SpinnerAdpter nationalitySpinner = new SpinnerAdpter(getContext(), lstNationalities);
+        SpinnerAdpter nationalitySpinner = new SpinnerAdpter(getContext(), Arrays.asList(country));
         spinnerSort.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = parent.getItemAtPosition(position).toString();
                 int Pos = Integer.parseInt(selectedItem);
-                txv_Nationality.setText(lstNationalities.get(Pos));
+                txv_Nationality.setText(country[Pos]);
 
             } // to close the onItemSelected
 
@@ -200,8 +203,8 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
         }
 
 
-        for (int i = 0; i < lstNationalities.size(); i++) {
-            if (lstNationalities.get(i).equalsIgnoreCase(AppConfig.getInstance().mUser.getNationality())) {
+        for (int i = 0; i < country.length; i++) {
+            if (country[i].equalsIgnoreCase(AppConfig.getInstance().mUser.getNationality())) {
                 spinnerSort.setSelection(i);
             } else  spinnerSort.setSelection(0);
         }
@@ -355,7 +358,7 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
 
     private boolean checkNationality() {
 //        if (edtGender.getText().toString().equalsIgnoreCase("Select Gender")) {
-        if (txv_Nationality.getText().toString().equalsIgnoreCase(lstNationalities.get(lstNationalities.size() - 1))) {
+        if (txv_Nationality.getText().toString().equalsIgnoreCase(country[(country.length - 1)])) {
 //            AppConfig.getInstance().showErrorMessage(getContext(), "City is not selected");
             Toast.makeText(getActivity(), "Nationality is not selected", Toast.LENGTH_SHORT).show();
             return false;
@@ -605,4 +608,75 @@ public class SignUpCompleteProfileFragment extends Fragment implements View.OnCl
         ft.commit();
     }
 
+
+
+    public static String[] country = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla",
+
+            "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria",
+
+            "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+
+            "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana",
+
+            "Brazil", "British Indian Ocean Territory", "British Virgin Islands", "Brunei", "Bulgaria",
+
+            "Burkina Faso", "Burma (Myanmar)", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+
+            "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island",
+
+            "Cocos (Keeling) Islands", "Colombia", "Comoros", "Cook Islands", "Costa Rica",
+
+            "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo",
+
+            "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+
+            "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
+
+            "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia",
+
+            "Gabon", "Gambia", "Gaza Strip", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
+
+            "Greenland", "Grenada", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
+
+            "Haiti", "Holy See (Vatican City)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India",
+
+            "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Ivory Coast", "Jamaica",
+
+            "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait",
+
+            "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
+
+            "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia",
+
+            "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mayotte", "Mexico",
+
+            "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco",
+
+            "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia",
+
+            "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea",
+
+            "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama",
+
+            "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland",
+
+            "Portugal", "Puerto Rico", "Qatar", "Republic of the Congo", "Romania", "Russia", "Rwanda",
+
+            "Saint Barthelemy", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin",
+
+            "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
+
+            "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone",
+
+            "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea",
+
+            "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland",
+
+            "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tokelau",
+
+            "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands",
+
+            "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "US Virgin Islands", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam",
+
+            "Wallis and Futuna", "West Bank", "Yemen", "Zambia", "Zimbabwe"};
 }
--- Denketas/app/src/main/java/com/armoomragames/denketa/Utils/AppConstt.java
+++ Denketas/app/src/main/java/com/armoomragames/denketa/Utils/AppConstt.java
@@ -26,7 +26,7 @@ public interface AppConstt {
         //Image url...
         String Artist_Image_Url = "https://art-station-bucket.s3.me-south-1.amazonaws.com/uploads_staging/";
         //DEBUG urls
-        String DEB_BASE_URL = "http://18.118.228.171:2000";
+        String DEB_BASE_URL = "http://18.119.55.236:2000";
         String DEB_URL_API = DEB_BASE_URL +"/api/v1/denetkas/";
 //        String DEB_URL_API = DEB_BASE_URL + "api/v1/mobile/";
 
@@ -39,7 +39,7 @@ public interface AppConstt {
 
 
         //release server
-        String REL_BASE_URL =  "http://18.118.228.171:2000/";//TODO also add staging+live FCM files+ Googlemaps(debug+release) to this project
+        String REL_BASE_URL =  "http://18.119.55.236:2000/";//TODO also add staging+live FCM files+ Googlemaps(debug+release) to this project
         String REL_URL_API = REL_BASE_URL + "/api/v1/denetkas/";
         String REL_BASE_URL_CHAT = REL_BASE_URL + "/chat";
         String REL_URL_IMG = REL_BASE_URL + "uploads/";
